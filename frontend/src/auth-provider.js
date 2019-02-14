b098b35a30eafdd1310766486aa618a3bce2f64f
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-02-14 18:52:42 +0000

    Include support for dummy confirmSignUp and resendCode

--- frontend/src/auth-provider.js
+++ frontend/src/auth-provider.js
@@ -6,25 +6,36 @@ export let Auth
 if (simulatedAuth) {
   console.log('Using simulated authentication')
 
+  function timerPromise(ms, reject = false) {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => (reject ? reject : resolve)(), ms)
+    )
+  }
+
   const authState = {}
   Auth = {
     signUp: ({ email }) => {
       Object.assign(authState, { email, loggedIn: false })
-      return new Promise(resolve => setTimeout(() => resolve(), 1200))
+      return timerPromise(1200)
     },
     signIn: ({ email }) => {
       Object.assign(authState, { email, loggedIn: true })
-      return new Promise(resolve => setTimeout(() => resolve(), 700))
+      return timerPromise(700)
     },
     currentSession: () => {
       return new Promise((resolve, reject) => {
-        const handler = authState.loggedIn ? resolve : reject
-        setTimeout(() => handler(), 400)
+        return timerPromise(400, !authState.loggedIn)
       })
+    },
+    confirmSignUp: () => {
+      return timerPromise(400)
     },
     signOut: () => {
       Object.assign(authState, { email: null, loggedIn: false })
-      return Promise.resolve()
+      return timerPromise(100)
+    },
+    resendSignUp: () => {
+      return timerPromise(100)
     }
   }
 } else {
