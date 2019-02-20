5d584e509d05f885d1c1015b1e59acb65da32879
Paul Kevany <pkevany25@gmail.com>
2019-02-20 16:31:48 +0000

    Add Item handler unit tests

---
+++ backend/test/services/checklists/items/addItem.test.js
@@ -0,0 +1,45 @@
+'use strict'
+const proxyquire = require('proxyquire')
+
+const { test } = require('tap')
+
+const received = {}
+const payload = { title: 'item1', value: '' }
+const addItemHandler = proxyquire(
+  '../../../../services/checklists/items/addItem.js',
+  {
+    '../items/items.js': {
+      addItem: params => {
+        received.addItemParams = params
+        return received
+      }
+    }
+  }
+)
+
+test('add item handler creates items for checklists', async t => {
+  const event = {
+    requestContext: {
+      identity: {
+        cognitoIdentityId: 'testUser'
+      }
+    },
+    body: JSON.stringify(payload),
+
+    pathParameters: {
+      id: '1234'
+    }
+  }
+
+  const result = await addItemHandler.main(event)
+
+  t.equal(
+    received.addItemParams.userId,
+    event.requestContext.identity.cognitoIdentityId
+  )
+  t.equal(received.addItemParams.listId, event.pathParameters.id)
+  t.equal(received.addItemParams.title, payload.title)
+  t.equal(received.addItemParams.value, payload.value)
+
+  t.end()
+})
