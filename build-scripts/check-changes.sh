fb372f16c787ff19966c83a9e3f5e832b4b98d83
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-03-01 08:09:52 +0000

    Debug check-changes.sh

--- build-scripts/check-changes.sh
+++ build-scripts/check-changes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-
+git log
 CHANGED_MODULES=$(git diff --name-only HEAD~1 HEAD | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq)
-echo CHANGED_MODULES=${CHANGED_MODULES}
+echo CHANGED_MODULES=${CHANGED_MODULES} > changed-modules.env
