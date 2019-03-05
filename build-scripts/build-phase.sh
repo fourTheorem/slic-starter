4ff7882145c9f608124741c75d3acece3ab689b5
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-03-05 06:40:46 +0000

    Add SLIC_STAGE=dev in right place in build scripts

--- build-scripts/build-phase.sh
+++ build-scripts/build-phase.sh
@@ -5,15 +5,15 @@ set -e
 source module-config.env
 
 run_build () {
+  export SLIC_STAGE=dev
+  source build-scripts/assume-cross-account-role.env
 	cd ${MODULE_NAME}
   if [ -e scripts/build.sh ]; then
     # Execute module-specific build
     bash scripts/build.sh
   fi
-  source build-scripts/assume-cross-account-role.env
   # TODO - Change stage to stg and prod as appropriate
   # (or each configured/injected stage)
-  export SLIC_STAGE=dev
   mkdir -p build-artifacts/${SLIC_STAGE}
   serverless package --package build-artifacts/${SLIC_STAGE} --stage ${SLIC_STAGE} -v
 }
