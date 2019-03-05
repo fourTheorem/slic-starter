a23d1bf4ae6b344758b9199f89a6ce416d07f167
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-03-05 06:31:10 +0000

    Allow module-specific build script (frontend build)

--- build-scripts/build-phase.sh
+++ build-scripts/build-phase.sh
@@ -5,8 +5,12 @@ set -e
 source module-config.env
 
 run_build () {
-  source build-scripts/assume-cross-account-role.env
 	cd ${MODULE_NAME}
+  if [ -e scripts/build.sh ]; then
+    # Execute module-specific build
+    bash scripts/build.sh
+  fi
+  source build-scripts/assume-cross-account-role.env
   # TODO - Change stage to stg and prod as appropriate
   # (or each configured/injected stage)
   export SLIC_STAGE=dev
