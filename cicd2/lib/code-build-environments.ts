f2629428578a505196ac1722994547000288e276
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-04-08 08:13:08 +0100

    Add CDK draft for codepipeline

---
+++ cicd2/lib/code-build-environments.ts
@@ -0,0 +1,9 @@
+import { ComputeType, LinuxBuildImage } from '@aws-cdk/aws-codebuild'
+
+export const defaultEnvironment = {
+  buildImage: LinuxBuildImage.fromDockerHub(
+    'eoinsha/codebuild-nodejs-serverless:8.11.0'
+  ),
+  computeType: ComputeType.Small,
+  privileged: false
+}
