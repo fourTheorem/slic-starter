25384608046e86f39f72872a01662aa0696bb3a3
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-04-08 12:12:52 +0100

    Add CodeBuild project for check changes

--- cicd2/lib/cicd2-stack.ts
+++ cicd2/lib/cicd2-stack.ts
@@ -8,6 +8,7 @@ import { SecretValue } from '@aws-cdk/cdk'
 
 import { defaultEnvironment } from './code-build-environments'
 import codePipelineActions = require('@aws-cdk/aws-codepipeline-actions')
+import CodeBuildRole from './code-build-role'
 import config from '../config'
 
 export class Cicd2Stack extends cdk.Stack {
@@ -26,18 +27,23 @@ export class Cicd2Stack extends cdk.Stack {
       new iam.PolicyStatement()
         .addActions('s3:DeleteObject')
         .addActions('s3:GetObject')
-        .addActions('s3:GetObjectVersioon')
+        .addActions('s3:GetObjectVersion')
         .addActions('s3:PutObject')
         .addResources(buildBucket.arnForObjects('*'))
-        .addAwsPrincipal(`${config.accountIds.dev}`)
+        .addAwsAccountPrincipal(`${config.accountIds.dev}`)
     )
     buildBucket.addToResourcePolicy(
       new iam.PolicyStatement()
         .addActions('s3:ListBucket')
-        .addActions('s3:GetBucket*')
+        .addActions('s3:GetEncryptionConfiguration')
         .addResources(buildBucket.bucketArn)
-        .addAwsPrincipal(`${config.accountIds.dev}`)
+        .addAwsAccountPrincipal(`${config.accountIds.dev}`)
     )
+    // TODO - Alter this deletion policy once testing is complete
+    const bucketResource = buildBucket.node.findChild(
+      'Resource'
+    ) as s3.CfnBucket
+    bucketResource.options.deletionPolicy = cdk.DeletionPolicy.Delete
 
     return {
       buildBucket
@@ -47,7 +53,10 @@ export class Cicd2Stack extends cdk.Stack {
   definePipeline(resources: any) {
     this.defineCodeBuildRole(resources)
 
-    const pipeline = new codePipeline.Pipeline(this, 'pipeline')
+    const pipeline = new codePipeline.Pipeline(this, 'pipeline', {
+      pipelineName: 'SLICPipeline'
+    })
+
     this.defineCodePipelineRole(resources, pipeline)
 
     const tokenSecret = SecretValue.secretsManager('CICD', {
@@ -87,7 +96,8 @@ export class Cicd2Stack extends cdk.Stack {
             }
           }
         },
-        environment: defaultEnvironment
+        environment: defaultEnvironment,
+        role: resources.codeBuildRole
       }
     )
 
@@ -105,112 +115,8 @@ export class Cicd2Stack extends cdk.Stack {
   }
 
   defineCodeBuildRole(resources: any) {
-    const codeBuildRole = new iam.Role(this, 'codeBuildRole', {
-      assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
-    })
-    // Allow CodeBuild to assume the deployment role in the target account
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addActions('sts:AssumeRole')
-        .addResource(
-          `arn:aws:iam::${config.accountIds.dev}:role/slic-cicd-deployment-role`
-        )
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('secretsmanager:GetSecretValue')
-        .addResource(
-          'arn:aws.secretsmanager:${AWS::Region}:${AWS::AccountId}:secret:CICD*'
-        )
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('s3:DeleteObject')
-        .addAction('s3:GetObject')
-        .addAction('s3:GetObjectVersion')
-        .addAction('s3:ListBucket')
-        .addAction('s3:PutObject')
-        .addAction('s3:GetBucketPolicy')
-        .addAction('s3:GetEncryptionConfiguration')
-        .addAction('s3:PutEncryptionConfiguration')
-        .addResources(`arn:aws:s3:::codepipeline-${config.region}-*`)
-        .addResources(resources.buildBucket.bucketArn)
-        .addResources(resources.buildBucket.arnForObjects('*'))
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('cloudformation:Describe*')
-        .addResource(
-          'arn:aws:cloudformation:${AWS::Region}:{AWS::AccountId}:stack/*/*'
-        )
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('lambda:Get*')
-        .addAction('lambda:List*')
-        .addAction('lambda:CreateFunction')
-        .addAllResources()
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('lambda:AddPermission')
-        .addAction('lambda:CreateAlias')
-        .addAction('lambda:DeleteFunction')
-        .addAction('lambda:InvokeFunction')
-        .addAction('lambda:PublishVersion')
-        .addAction('lambda:RemovePermission')
-        .addAction('lambda:Update*')
-        .addResource(`arn:aws:labmda:${config.region}:*:function:*`)
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('apigateway:GET')
-        .addAction('apigateway:POST')
-        .addAction('apigateway:PUT')
-        .addAction('apigateway:DELET')
-        .addResource('arn:aws:apigateway:*::/restapis*')
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('iam:PassRole')
-        .addAction('iam:GetRole')
-        .addAction('iam:CreateRole')
-        .addAction('iam:PutRolePolicy')
-        .addAction('iam:DeleteRolePolicy')
-        .addAction('iam:DeleteRole')
-        .addResource('arn:aws:iam::*:role/*')
-    ) // TODO - Change to specific Lambda roles?
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('sqs:*')
-        .addResource('arn:aws:sqs:*:*:*')
-    )
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('logs:CreateLogGroup')
-        .addAction('logs:CreateLogStream')
-        .addAction('logs:DeleteLogGroup')
-        .addAction('logs:PutLogEvents')
-        .addAllResources()
-    ) // TODO - specific log groups
-    codeBuildRole.addToPolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addAction('events:Put*')
-        .addAction('events:Remove*')
-        .addAction('events:Delete*')
-        .addAllResources()
-    ) // TODO - specific events
+    const codeBuildRole = new CodeBuildRole(this, resources, 'codeBuildRole')
+    resources.codeBuildRole = codeBuildRole
   }
 
   defineCodePipelineRole(resources: any, pipeline: Pipeline) {
@@ -233,21 +139,6 @@ export class Cicd2Stack extends cdk.Stack {
 
     pipeline.addToRolePolicy(
       new iam.PolicyStatement()
-        .addActions('s3:CreateBucke')
-        .addActions('s3:GetEncryptionConfiguration')
-        .addActions('s3:SetEncryptionConfiguration')
-        .addAllResources()
-    )
-
-    pipeline.addToRolePolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addActions('sns:Publish')
-        .addResources(resources.buildNotificationTopic)
-    )
-
-    pipeline.addToRolePolicy(
-      new iam.PolicyStatement()
         .allow()
         .addActions('cloudformation:List*')
         .addActions('cloudformation:Get*')
@@ -258,15 +149,6 @@ export class Cicd2Stack extends cdk.Stack {
         .addActions('cloudformation:DeleteStack')
         .addActions('cloudformation:Describe*')
         .addActions('cloudformation:UpdateStack')
-        .addAllResources()
-    )
-
-    pipeline.addToRolePolicy(
-      new iam.PolicyStatement()
-        .allow()
-        .addActions('codebuild:StartBuild')
-        .addActions('codebuild:BatchGetBuilds')
-        .addActions('codebuild:StopBuild')
         .addAllResources()
     )
 
