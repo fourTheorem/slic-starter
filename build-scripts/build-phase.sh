29dd7883c9d1d6e729d596fda435a515dfb4ffbf
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-04-12 05:51:37 +0100

    Build for stg and prod, assume cross account role first

--- build-scripts/build-phase.sh
+++ build-scripts/build-phase.sh
@@ -5,12 +5,12 @@ set -e
 source module-config.env
 
 run_build () {
+  source build-scripts/assume-cross-account-role.env
 	cd ${MODULE_NAME}
   if [ -e scripts/build.sh ]; then
     # Execute module-specific build
     bash scripts/build.sh
   fi
-  source build-scripts/assume-cross-account-role.env
   echo "Packaging for SLIC_STAGE ${SLIC_STAGE}"
   mkdir -p build-artifacts/${SLIC_STAGE}
   SLIC_STAGE=${SLIC_STAGE} serverless package --package build-artifacts/${SLIC_STAGE} --stage ${SLIC_STAGE} -v
