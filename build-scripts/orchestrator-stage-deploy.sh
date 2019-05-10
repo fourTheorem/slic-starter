adeba37fb61caaf458f0682ecbe0d891fd54b425
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-05-10 16:45:23 +0100

    Trigger services builds with S3

--- build-scripts/orchestrator-stage-deploy.sh
+++ build-scripts/orchestrator-stage-deploy.sh
@@ -7,8 +7,25 @@ env
 echo PWD
 pwd
 
-echo FIND
-find . -print
-
 echo PIPELINE STATE
 cat pipeline-state.env
+
+source pipeline-state.env
+
+CODEBUILD_SOURCE_VERSION=arn:aws:s3:::slic-build-artifacts-285982925560-eu-west-1/OrchestratorPipeline/Artifact_S/OQofDC1.zip 
+S3_FULL_PATH=$(echo ${CODEBUILD_SOURCE_VERSION} | awk -F ':' '{print $6}')
+S3_BUCKET_NAME=$(echo ${S3_FULL_PATH} | sed -e 's/\([^\/]*\)\/.*/\1/')
+ARTIFACT_OBJECT_KEY=$(echo $S3_FULL_PATH| sed -e 's/[^\/]*\/\(.*\)/\1/')
+
+bash -version
+
+for moduleName in ${MODULE_NAMES}; do
+  if ${changedModules[${moduleName}]}; then
+    echo Copying artifact to trigger build of ${moduleName}
+    CMD="aws s3 cp s3://${S3_BUCKET_NAME}/${ARTIFACT_OBJECT_KEY} s3://${S3_BUCKET_NAME}/${SLIC_STAGE}_module_pipelines/module_source/${moduleName}.zip"
+    echo Executing ${CMD}
+    ${CMD}
+  else 
+    echo Skipping unchanged module ${moduleName}
+  fi
+done
