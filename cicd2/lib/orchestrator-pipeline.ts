4ec5eedc5d957a33a140c9e7b011287c117bd9b6
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-05-13 11:56:35 +0100

    Correct set -e'

--- cicd2/lib/orchestrator-pipeline.ts
+++ cicd2/lib/orchestrator-pipeline.ts
@@ -1,3 +1,4 @@
+import { Role, ServicePrincipal, PolicyStatement } from '@aws-cdk/aws-iam'
 import { Pipeline, PipelineProps, Artifact } from '@aws-cdk/aws-codepipeline'
 import { Construct } from '@aws-cdk/cdk'
 import { Bucket } from '@aws-cdk/aws-s3'
@@ -6,7 +7,7 @@ import {
   CodeBuildAction
 } from '@aws-cdk/aws-codepipeline-actions'
 import StageName from './stage-name'
-import { OrchestratorDeployProject } from './orchestrator-deploy-project'
+import { OrchestratorDeployProject } from './projects/orchestrator-deploy-project'
 import { SLIC_PIPELINE_SOURCE_ARTIFACT } from './projects/source-project'
 
 export interface OrchestratorPipelineProps extends PipelineProps {
@@ -21,6 +22,20 @@ export class OrchestratorPipeline extends Pipeline {
       artifactBucket: artifactsBucket,
       ...rest
     })
+
+    const orchestratorCodeBuildRole = new Role(
+      this,
+      'orchestrator-codebuild-role',
+      {
+        assumedBy: new ServicePrincipal('codebuild.amazonaws.com')
+      }
+    )
+    orchestratorCodeBuildRole.addToPolicy(
+      new PolicyStatement()
+        .addActions('codepipeline:GetPipelineExecution')
+        .addActions('codepipeline:StartPipelineExecution')
+        .addAllResources()
+    )
 
     const sourceOutputArtifact = new Artifact()
 
@@ -41,7 +56,8 @@ export class OrchestratorPipeline extends Pipeline {
       this,
       'orchestratorDeployStaging',
       {
-        stageName: StageName.stg
+        stageName: StageName.stg,
+        role: orchestratorCodeBuildRole
       }
     )
 
