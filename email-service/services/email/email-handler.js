b60fd680fd3fe3d95d039329b81bfa3d271279bd
Paul Kevany <pkevany25@gmail.com>
2019-05-14 16:14:42 +0100

    Implement sendEmail functionality

---
+++ email-service/services/email/email-handler.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk')
+
+const log = require('../../lib/log')
+
+async function sendEmail(message) {
+  const params = {
+    Destination: {
+      ToAddresses: ['paul.kevany@fourtheorem.com']
+    },
+
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: 'Welcome to SLIC Starter'
+        }
+      },
+
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'SLIC Updates'
+      }
+    },
+
+    Source: 'paul.kevany@fourtheorem.com'
+  }
+
+  const result = await new AWS.SES().sendEmail(params).promise()
+
+  log.info({ result }, 'Sent email')
+}
+
+module.exports = {
+  sendEmail
+}
