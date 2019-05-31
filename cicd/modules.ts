50b102ccd79541075e1d39ff4f4547332731a14b
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-05-31 17:26:13 +0100

    Add new service, simplify modules.ts (remove ordering)

--- cicd/modules.ts
+++ cicd/modules.ts
@@ -1,53 +1,17 @@
-// @ts-ignore
-import tsort = require('tsort')
-
 export interface Modules {
-  stages: Array<string[]>
-  deployOrder: {
-    [moduleName: string]: number
-  }
   moduleNames: string[]
-  dependencies: {
-    [moduleName: string]: string[]
-  }
 }
 
 const modules: Modules = {
-  moduleNames: ['frontend', 'backend', 'baseline'],
-  dependencies: {
-    backend: ['baseline'],
-    frontend: ['baseline']
-  },
-  stages: [],
-  deployOrder: {}
+  moduleNames: [
+    'frontend',
+    'backend',
+    'baseline',
+    'logging',
+    'user-service',
+    'email-service',
+    'welcome-service'
+  ]
 }
+
 export default modules
-
-// Turn dependencies into an optimal deploy order of stages
-const graph = tsort()
-Object.entries(modules.dependencies).forEach(([key, deps]) => {
-  deps.forEach(dep => graph.add(dep, key))
-})
-const order = graph.sort()
-
-modules.stages = []
-let stage = []
-
-while (order.length) {
-  const mod = order.shift()
-  stage.push(mod)
-  const deps = (order.length && modules.dependencies[order[0]]) || []
-  if (deps.indexOf(mod) > -1) {
-    modules.stages.push(stage)
-    stage = []
-  }
-}
-if (stage.length) {
-  modules.stages.push(stage)
-}
-
-modules.stages.forEach((stage, index) => {
-  stage.forEach(mod => {
-    modules.deployOrder[mod] = index + 1
-  })
-})
