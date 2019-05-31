3bb0eadc4440cbf87562ab72b006d40d57e50bbd
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-05-31 14:23:00 +0100

    Conclude user service unit tests

--- user-service/test/services/users/user.test.js
+++ user-service/test/services/users/user.test.js
@@ -3,28 +3,32 @@
 const path = require('path')
 const awsMock = require('aws-sdk-mock')
 const { test } = require('tap')
-const userId = 'Test-User'
 
 awsMock.setSDK(path.resolve('./node_modules/aws-sdk'))
 process.env.USER_POOL_ID = 'User_pool123'
 
-const received = {
-  cognito: {}
-}
+const testEmail = 'email@example.com'
 
 const uid = {
   userId: 'userId'
 }
 
+awsMock.mock('SSM', 'getParameter', function(params, callback) {
+  callback(null, { Parameter: { Value: 'test-user-pool-id' } })
+})
+
 awsMock.mock('CognitoIdentityServiceProvider', 'adminGetUser', function(
   params,
   callback
 ) {
-  const UserAttributes = [{ Name: 'email', Value: 'email@example.com' }]
+  const UserAttributes = [{ Name: 'email', Value: testEmail }]
   callback(null, { UserAttributes })
 })
 
-test('adminGetUser retrieves cognito user information', async t => {
+test('user service retrieves cognito user information', async t => {
   const userService = require('../../../services/users/user')
   const response = await userService.get(uid)
+  t.match(response, {
+    email: testEmail
+  })
 })
