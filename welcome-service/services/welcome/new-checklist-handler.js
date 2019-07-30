ff10c603f04049e1827da1e7a3a4383dbbd73fea
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-07-30 13:06:01 +0100

    Rectify tests for welcome service

--- welcome-service/services/welcome/new-checklist-handler.js
+++ welcome-service/services/welcome/new-checklist-handler.js
@@ -1,14 +1,9 @@
 'use strict'
-const awsXray = require('aws-xray-sdk')
-const AWS = require('aws-sdk')
 
-const log = require('slic-tools/log')
 const { getUser } = require('slic-tools/user-util')
 const { sendEmail } = require('slic-tools/email-util')
 
 async function handleNewChecklist(event) {
-  log.info({ event })
-
   const checklist = event.detail
   const { userId, name } = checklist
 
--- welcome-service/test/services/welcome/welcome.test.js
+++ welcome-service/test/services/welcome/welcome.test.js
@@ -1,46 +1,19 @@
 'use strict'
 
-const path = require('path')
-const proxyquire = require('proxyquire').noPreserveCache()
+const uuid = require('uuid')
 
-const awsMock = require('aws-sdk-mock')
+const proxyquire = require('proxyquire').noPreserveCache()
 const { test } = require('tap')
 
-const userId = 'Test-User'
-
-awsMock.setSDK(path.resolve('./node_modules/aws-sdk'))
-
-const received = {
-  SQS: {}
+const userId = uuid.v4()
+const testUser = {
+  userId,
+  email: 'test-user@example.com'
 }
 
-awsMock.mock('SQS', 'sendMessage', function(params, callback) {
-  received.SQS.sendMessage = params
-  callback(null, { ...params })
-})
+const received = {}
 
-awsMock.mock('SQS', 'getQueueUrl', function(params, callback) {
-  received.SQS.getQueueUrl = params
-  callback(null, { QueueUrl: 'test-queue-url' })
-})
-
-awsMock.mock('SSM', 'getParameter', function(params, callback) {
-  callback(null, {
-    Parameter: {
-      Value: 'http://userservice.example.com/'
-    }
-  })
-})
-
-test('handleNewChecklist requires queue name to be set', t => {
-  delete process.env.EMAIL_QUEUE_NAME
-  t.throws(() => {
-    require('../../../services/welcome/new-checklist-handler')
-  })
-  t.end()
-})
-
-test('handleNewChecklist sends a message to an SQS queue', async t => {
+test('handleNewChecklist sends an email message', async t => {
   const event = {
     detail: {
       userId,
@@ -48,25 +21,28 @@ test('handleNewChecklist sends a message to an SQS queue', async t => {
     }
   }
 
-  process.env.EMAIL_QUEUE_NAME = 'test-queue-name'
   const checklistHandler = proxyquire(
     '../../../services/welcome/new-checklist-handler',
     {
-      'aws-signed-axios': args =>
-        Promise.resolve({ data: { email: 'test@example.com' } }),
-      '@noCallThru': true
+      'slic-tools/user-util': {
+        getUser: () => Promise.resolve(testUser),
+        '@noCallThru': true
+      },
+      'slic-tools/email-util': {
+        sendEmail: (...args) => {
+          received.sendEmailArgs = args
+        },
+        '@noCallThru': true
+      }
     }
   )
 
   await checklistHandler.handleNewChecklist(event)
 
-  const sqsParams = received.SQS.sendMessage
-  const parsedBody = JSON.parse(sqsParams.MessageBody)
-
-  t.equal(parsedBody.to, 'test@example.com')
-  t.equal(parsedBody.subject, 'Your SLIC List')
-  t.ok(parsedBody.body)
-  t.ok(parsedBody.body.indexOf('New Checklist') > -1)
-
+  t.match(received.sendEmailArgs[0], {
+    to: testUser.email,
+    subject: 'Your SLIC List'
+  })
+  t.ok(received.sendEmailArgs[0].body)
   t.end()
 })
