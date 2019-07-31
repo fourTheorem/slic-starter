db07e3494244d11c2d8cab00ec746ea1b431570b
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-07-31 17:49:40 +0100

    Use middy for user service URL

--- slic-tools/user-util.js
+++ slic-tools/user-util.js
@@ -1,19 +1,9 @@
 'use strict'
 
-const AWS = require('aws-sdk')
-const awsXray = require('aws-xray-sdk')
 const signedAxios = require('aws-signed-axios')
-const SSM = awsXray.captureAWSClient(
-  new AWS.SSM({ endpoint: process.env.SSM_ENDPOINT_URL })
-)
-
-const log = require('./log')
-const userServiceUrlPromise = fetchUserServiceUrl()
 
 async function getUser(userId) {
-  const userUrl = `${await getUserServiceUrl()}${userId}`
-
-  debugger
+  const userUrl = `${process.env.USER_SERVICE_URL}${userId}`
 
   const { data: result } = await signedAxios({
     method: 'GET',
@@ -22,21 +12,6 @@ async function getUser(userId) {
   return result
 }
 
-function getUserServiceUrl() {
-  return userServiceUrlPromise
-}
-
-async function fetchUserServiceUrl() {
-  const params = {
-    Name: `/${process.env.SLIC_STAGE}/user-service/url`
-  }
-
-  const result = await SSM.getParameter(params).promise()
-  console.log({ result }, 'User service URL retrieved')
-  return result.Parameter.Value
-}
-
 module.exports = {
-  getUser,
-  getUserServiceUrl
+  getUser
 }
