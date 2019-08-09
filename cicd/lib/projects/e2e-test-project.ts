f48b91b31b654a72083103b85e3b403cd0334350
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-08-09 09:28:33 +0100

    Use specific Nodejs version (8) in CodeBuild

--- cicd/lib/projects/e2e-test-project.ts
+++ cicd/lib/projects/e2e-test-project.ts
@@ -2,7 +2,8 @@ import {
   PipelineProject,
   PipelineProjectProps,
   BuildEnvironmentVariableType,
-  BuildSpec
+  BuildSpec,
+  LinuxBuildImage
 } from '@aws-cdk/aws-codebuild'
 import StageName from '../stage-name'
 import { Construct } from '@aws-cdk/core'
@@ -36,6 +37,9 @@ export class E2ETestProject extends PipelineProject {
 
     super(scope, id, {
       projectName: `${props.stageName}E2ETest`,
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_2_0
+      },
       environmentVariables: {
         SLIC_STAGE: {
           type: BuildEnvironmentVariableType.PLAINTEXT,
