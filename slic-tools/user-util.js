7f31ab9dd30e14c410456b211958bfa25bcd3893
Paul Kevany <pkevany25@gmail.com>
2019-08-13 14:47:32 +0100

    Add error handling in user-util

--- slic-tools/user-util.js
+++ slic-tools/user-util.js
@@ -2,6 +2,8 @@
 
 const signedAxios = require('aws-signed-axios')
 
+const log = require('./log')
+
 async function getUser(userId) {
   const userServiceUrl = process.env.USER_SERVICE_URL
   if (!userServiceUrl) {
@@ -9,11 +11,19 @@ async function getUser(userId) {
   }
 
   const userUrl = `${userServiceUrl}${userId}`
-  const { data: result } = await signedAxios({
-    method: 'GET',
-    url: userUrl
-  })
-  return result
+  try {
+    const { data: result } = await signedAxios({
+      method: 'GET',
+      url: userUrl
+    })
+    return result
+  } catch(err) {
+    const { data, status, headers } = err.response || {}
+    if (status) {
+      log.error({ data, status, headers }, 'Error retrieving user')
+    }
+    throw err
+  }
 }
 
 module.exports = {
