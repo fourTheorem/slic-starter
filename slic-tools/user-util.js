6dcf9d93adf84147fb2146d7d65bd6b02d099014
Paul Kevany <pkevany25@gmail.com>
2019-08-13 11:34:56 +0100

    Add error handling for missing user-service-url

--- slic-tools/user-util.js
+++ slic-tools/user-util.js
@@ -3,8 +3,12 @@
 const signedAxios = require('aws-signed-axios')
 
 async function getUser(userId) {
-  const userUrl = `${process.env.USER_SERVICE_URL}${userId}`
+  const userServiceUrl = process.env.USER_SERVICE_URL
+  if (!userServiceUrl) {
+    throw new Error('USER_SERVICE_URL is not defined')
+  }
 
+  const userUrl = `${userServiceUrl}${userId}`
   const { data: result } = await signedAxios({
     method: 'GET',
     url: userUrl
