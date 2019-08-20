57073ed4901e33c7b7611ed72a62bb0f586f96ed
Paul Kevany <pkevany25@gmail.com>
2019-08-20 08:36:57 +0100

    Include API and Certs modules in build if domain is provided

--- cicd/modules.ts
+++ cicd/modules.ts
@@ -1,34 +1,20 @@
 export interface Modules {
   moduleNames: string[]
 }
-let modules
-if (process.env.SLIC_NS_DOMAIN) {
-  modules: Modules = {
-    moduleNames: [
-      'api-service',
-      'frontend',
-      'certs',
-      'logging',
-      'checklist-service',
-      'user-service',
-      'email-service',
-      'welcome-service',
-      'sharing-service'
-    ]
-  }
-} else {
-  modules: Modules = {
-    moduleNames: [
-      'api-service',
-      'frontend',
-      'logging',
-      'checklist-service',
-      'user-service',
-      'email-service',
-      'welcome-service',
-      'sharing-service'
-    ]
-  }
+
+const moduleNames = [
+  'frontend',
+  'logging',
+  'checklist-service',
+  'user-service',
+  'email-service',
+  'welcome-service',
+  'sharing-service',
+  ...(process.env.SLIC_NS_DOMAIN ? ['certs', 'api-service'] : [])
+]
+
+const modules: Modules = {
+  moduleNames
 }
 
 export default modules
