278e5c2994d4373d9359496b95d87bce00162717
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-03-15 20:52:26 +0000

    Update localstack/serverless-offline configs for new versions

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,13 +4,13 @@ module.exports = function (app) {
   app.use(
     proxy('/checklist', {
       target: 'http://localhost:4000',
-      pathRewrite: { '^/checklist': '' }
+      pathRewrite: { '^/checklist': '/local' }
     })
   )
   app.use(
     proxy('/share', {
-      target: 'http://localhost:4004',
-      pathRewrite: { '^/share': '' }
+      target: 'http://localhost:4006',
+      pathRewrite: { '^/share': '/local' }
     })
   )
 }
