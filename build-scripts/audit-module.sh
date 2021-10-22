926d1dab20a02b195422a8964a73feba923c6909
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-10-22 21:32:45 +0100

    Update package versions on audit

--- build-scripts/audit-module.sh
+++ build-scripts/audit-module.sh
@@ -5,14 +5,14 @@ set -e
 npm install -g npm@latest
 
 if [ -e package.json ]; then
-  npm audit --audit-level=moderate
+  npm audit --audit-level=high
 else
   echo No root package.json, skipping audit
 fi
 
 cd ${MODULE_NAME}
 if [ -e package.json ]; then
-  npm audit --audit-level=moderate
+  npm audit --audit-level=high
 else
   echo No module package.json, skipping audit
 fi
