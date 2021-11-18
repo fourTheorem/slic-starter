3858681e7c6086a88adedb52ad1670c7e3778cbd
Diren AKKOC DEMIR <74667729+direnakkocdemir@users.noreply.github.com>
2021-11-18 12:40:50 +0000

    create a workflow for docs that allows deployments to happen through Github Actions

---
+++ docs-build/src/components/HomepageFeatures.js
@@ -0,0 +1,67 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import clsx from 'clsx'
+import styles from './HomepageFeatures.module.css'
+
+const FeatureList = [
+  {
+    title: 'Easy to Start',
+    Svg: require('../../static/img/lambda.jpg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    )
+  },
+  {
+    title: 'Easy to Build',
+    Svg: require('../../static/img/cloudFront.jpg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    )
+  },
+  {
+    title: 'Easy to Deploy',
+    Svg: require('../../static/img/route53.jpg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    )
+  }
+]
+
+// eslint-disable-next-line no-unused-vars
+function Feature ({ Svg, title, description }) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={Svg} className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default function HomepageFeatures () {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
