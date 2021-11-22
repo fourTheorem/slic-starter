4b105f9938ae387b86d1e9601aef0632f4fb9e0f
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-11-22 06:36:36 +0000

    Add cache for synth and test

--- cicd2/lib/pipeline-stack.ts
+++ cicd2/lib/pipeline-stack.ts
@@ -1,29 +1,45 @@
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 
+import * as s3 from '@aws-cdk/aws-s3'
 import * as pipelines from '@aws-cdk/pipelines';
 import * as codebuild from '@aws-cdk/aws-codebuild'
+import * as iam from '@aws-cdk/aws-iam'
 
 import config from '../config';
-import { CodeBuildStep } from '@aws-cdk/pipelines';
 
 export class PipelineStack extends Stack {
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      synth: new pipelines.ShellStep('Synth', {
-        // This relies on the presence of a Secrets Manager secret named 'github-token'
-        input: pipelines.CodePipelineSource.gitHub(
-          `${config.sourceRepoOwner}/${config.sourceRepoName}`, config.sourceBranch, {
-            authentication: SecretValue.secretsManager('github-token')
-          }
-        ),
-        commands: ['cd cicd2', 'npm ci', 'npm run build', 'npm run cdk -- synth'],
-        primaryOutputDirectory: 'cicd2/cdk.out',
+    const cacheBucket = new s3.Bucket(this, 'CodeBuildCache')
+    const cacheAccessStatement = new iam.PolicyStatement({
+      actions: ['s3:GetObject', 's3:PutObject'],
+      effect: iam.Effect.ALLOW,
+      resources: [`${cacheBucket.bucketArn}/codeBuildCache/*`]
+    })
+
+    const synthStep = new pipelines.CodeBuildStep('Synth', {
+      rolePolicyStatements: [cacheAccessStatement],
+      // This relies on the presence of a Secrets Manager secret named 'github-token'
+      input: pipelines.CodePipelineSource.gitHub(
+        `${config.sourceRepoOwner}/${config.sourceRepoName}`, config.sourceBranch, {
+        authentication: SecretValue.secretsManager('github-token')
+      }),
+      commands: ['cd cicd2', 'npm ci', 'npm run build', 'npm run cdk -- synth'],
+      primaryOutputDirectory: 'cicd2/cdk.out',
+      partialBuildSpec: codebuild.BuildSpec.fromObject({
+        cache: {
+          paths: ['cicd2/cdk.out/**/*', 'cicd2/node_modules/**/*']
+        },
       })
     })
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      synth: synthStep
+    })
+
     const unitTestStep = new pipelines.CodeBuildStep('UnitTest', {
+      rolePolicyStatements: [cacheAccessStatement],
       installCommands: [
         'bash util/install-packages.sh'
       ],
@@ -41,15 +57,16 @@ export class PipelineStack extends Stack {
       pre: [unitTestStep]
     })
 
-    pipeline.buildPipeline()
+    pipeline.buildPipeline() // Build so we can access the CfnProject to inject cache property
 
-    const artifactBucketName = pipeline.pipeline.artifactBucket.bucketName
-
-    const cbProject = unitTestStep.project.node.defaultChild as codebuild.CfnProject
-    cbProject.cache = {
+    const projectCacheConfig = {
       type: 'S3',
-      location: `${artifactBucketName}/codeBuildCache`
+      location: `${cacheBucket.bucketName}/codeBuildCache`
     }
+    const synthProject = unitTestStep.project.node.defaultChild as codebuild.CfnProject
+    const utProject = unitTestStep.project.node.defaultChild as codebuild.CfnProject
+    synthProject.cache = projectCacheConfig
+    utProject.cache = projectCacheConfig
   }
 
 }
