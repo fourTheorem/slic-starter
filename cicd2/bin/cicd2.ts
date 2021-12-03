f680897c2757e0fc0beaad288d1a67e6376ef973
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-12-03 07:53:25 +0000

    Try multiple projects in a step

--- cicd2/bin/cicd2.ts
+++ cicd2/bin/cicd2.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { Cicd2Stack } from '../lib/cicd2-stack';
+import { PipelineStack } from '../stacks/pipeline-stack';
 
 const app = new cdk.App();
-new Cicd2Stack(app, 'Cicd2Stack', {
+new PipelineStack(app, 'PipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
