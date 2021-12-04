41418bc9cfa2da41157d726b5a4960dc09e592fb
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-12-04 10:18:17 +0000

    Add support for stages (dev or stg,prod)

--- cicd2/bin/cicd2.ts
+++ cicd2/bin/cicd2.ts
@@ -5,20 +5,36 @@ import * as iam from '@aws-cdk/aws-iam';
 import { PipelineStack } from '../stacks/pipeline-stack';
 import { CrossAccountStack } from '../stacks/cross-account-stack';
 
-const stage = 'dev' // TODO
-
 const app = new cdk.App();
-const crossAccountStack = new CrossAccountStack(app, 'CrossAccountStack', {
-  env: {
-    account: app.node.tryGetContext('target-account') || app.region,
-    region: app.node.tryGetContext('target-region') || app.region
-  }
-})
+const stagesInput = app.node.tryGetContext('stages')
+if (!stagesInput) {
+  throw new Error('Stages need to be specified in CDK context. ' +
+                  'Pass -c stages=dev or -c stages=stg,prd to cdk deploy. ' +
+                  'CDK context variables can also be passed in cdk.json. ' +
+                  'See https://docs.aws.amazon.com/cdk/latest/guide/get_context_var.html')
+}
+const stages = stagesInput.split(',')
+const [lastStage] = stages.slice(-1)
+
+const crossAccountDeployRoles: {[key: string]: iam.IRole} = {}
+
+for (const stage of stages) {
+  const crossAccountStack = new CrossAccountStack(app, `${stage}CrossAccountStack`, {
+    env: {
+      account: app.node.tryGetContext(`${stage}-account`) || app.region,
+      region: app.node.tryGetContext(`${stage}-region`) || app.region
+    },
+    stage
+  })
+  crossAccountDeployRoles[stage] = crossAccountStack.crossAccountDeployRole
+}
 
 new PipelineStack(app, 'PipelineStack', {
   env: {
     account: app.node.tryGetContext('deploy-account') || app.region,
     region: app.node.tryGetContext('deploy-region') || app.region
   },
-  crossAccountDeployRole: crossAccountStack.crossAccountDeployRole
-});
+  crossAccountDeployRoles,
+  stages,
+  stackName:`${lastStage}PipelineStack`
+})
