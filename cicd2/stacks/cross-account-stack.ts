cdc1547b843998622dc81e065fa5405453b456f1
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-12-04 10:18:17 +0000

    Add support for stages (dev or stg,prod)

--- cicd2/stacks/cross-account-stack.ts
+++ cicd2/stacks/cross-account-stack.ts
@@ -2,17 +2,33 @@ import { Construct, PhysicalName, Stack, StackProps } from '@aws-cdk/core'
 
 import * as iam from '@aws-cdk/aws-iam'
 
+interface CrossAccountStackProps extends StackProps {
+  stage: string
+}
+
 export class CrossAccountStack extends Stack {
 
   readonly crossAccountDeployRole: iam.IRole
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: CrossAccountStackProps) {
     super(scope, id, props)
-    const stage = 'dev' // TODO - change
+    const trustPrincipals: iam.PrincipalBase[] = []
+    const stage = props.stage
+    if (stage.startsWith('dev') || stage.endsWith('dev')) {
+      // Allow users in devs account to assume the deployment role to deploy manually
+      // with fast `sls deploy`, for example. Should not be enabled for other targets where
+      // deployment should always be automated.
+      trustPrincipals.push(new iam.AccountPrincipal(this.account))
+    }
 
+    const deployAccount = this.node.tryGetContext('deploy-account')
+    if (deployAccount) {
+      trustPrincipals.push(new iam.AccountPrincipal(deployAccount))
+    }
+    
     this.crossAccountDeployRole = new iam.Role(this, `${stage}CrossAccountDeployRole`, {
       roleName: `${stage}CrossAccountDeployRole`,
-      assumedBy: new iam.AccountPrincipal(this.node.tryGetContext('deploy-account') || this.account),
+      assumedBy: new iam.CompositePrincipal(...trustPrincipals)
     })
     const services = [
       'cloudformation',
--- cicd2/stacks/pipeline-stack.ts
+++ cicd2/stacks/pipeline-stack.ts
@@ -10,14 +10,17 @@ import config from '../config'
 import modules from '../modules'
 
 interface PipelineStackProps extends StackProps {
-  crossAccountDeployRole: iam.IRole  
+  crossAccountDeployRoles: {[key: string]: iam.IRole},
+  stages: string[] 
 }
 
 export class PipelineStack extends Stack {
 
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props)
-    const stage = 'dev' // TODO - change
+
+    const stages = props.stages
+    const lastStage = stages.slice(-1)
 
     const codeBuildEnvironment = {
       computeType: codeBuild.ComputeType.LARGE,
@@ -25,8 +28,8 @@ export class PipelineStack extends Stack {
     }
 
     const artifactBucket = new s3.Bucket(this, 'ArtifactBucket')
-    const pipeline = new codePipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: `${config.sourceRepoName}-${stage}`,
+    const pipeline = new codePipeline.Pipeline(this, `pipeline_${lastStage}`, {
+      pipelineName: `${config.appName}-${lastStage}`,
       artifactBucket,
       restartExecutionOnUpdate: true // Allow the pipeline to restart if it mutates during CDK deploy
     })
@@ -46,9 +49,10 @@ export class PipelineStack extends Stack {
       actions: [sourceAction],
     })
 
+    const stagesContextArg = `--context stages=${stages.join(',')}`
     const synthArtifact = new codePipeline.Artifact()
     const synthProject = new codeBuild.PipelineProject(this, 'CdkSynthPipeline', {
-      projectName: `${config.appName}-${stage}-cdk-synth`,
+      projectName: `${config.appName}-${lastStage}-cdk-synth`,
       buildSpec: codeBuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
@@ -56,13 +60,12 @@ export class PipelineStack extends Stack {
             commands: ['cd cicd2', 'npm ci']
           },
           build: {
-            commands: ['npm run build', 'npm run cdk -- synth']
+            commands: ['npm run build', `npm run cdk -- synth ${stagesContextArg}`]
           },
         },
         artifacts: {
           'files': ['cicd2/**/*', 'app.yml'],
-          'name': `synth-${stage}-$(date +%Y%m%d%H%M%S)-$BUILD_ID`,
-          'enable-symlinks': true,
+          'enable-symlinks': true,  // important for node_modules/.bin links, like `cdk` itself!
         }
       }),
       environment: codeBuildEnvironment
@@ -78,43 +81,57 @@ export class PipelineStack extends Stack {
       })]
     })
 
-    const targetAccount = this.node.tryGetContext('target-account') || this.account
-    const targetRegion = this.node.tryGetContext('target-region') || this.region
     const deployAccount = this.node.tryGetContext('deploy-account') || this.account
     const deployRegion = this.node.tryGetContext('deploy-region') || this.region
 
     const cdkContextArgs = [
-      `--context target-account=${targetAccount}`,
-      `--context target-region=${targetRegion}`,
+      stagesContextArg,
       `--context deploy-account=${deployAccount}`,
       `--context deploy-region=${deployRegion}`,
-    ].join(' ')
+    ]
+
+    const targetAccounts = new Set()
+    for (const stage of stages) {
+      const targetAccount = this.node.tryGetContext(`${stage}-account`) || this.account
+      targetAccounts.add(targetAccount)
+      const targetRegion = this.node.tryGetContext(`${stage}-region`) || this.region
+      cdkContextArgs.push(...[
+        `--context ${stage}-account=${targetAccount}`,
+        `--context ${stage}-region=${targetRegion}`,
+      ])
+    }
 
     const cdkDeployProject = new codeBuild.PipelineProject(this, 'CdkDeployPipeline', {
-      projectName: `${config.appName}-${stage}-cdk-deploy`,
+      projectName: `${config.appName}-${lastStage}-cdk-deploy`,
       buildSpec: codeBuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
           build: {
             commands: [
               'cd cicd2',
-              `npm run cdk -- deploy --require-approval=never --verbose ${cdkContextArgs} CrossAccountStack`,
-              `npm run cdk -- deploy --require-approval=never --verbose ${cdkContextArgs} PipelineStack`
+              `npm run cdk -- deploy --require-approval=never --verbose ${cdkContextArgs.join(' ')} PipelineStack`
             ]
           },
         },
       }),
       environment: codeBuildEnvironment,
     })
+
     cdkDeployProject.role?.addToPrincipalPolicy(new iam.PolicyStatement({
       actions: ['cloudformation:DescribeStacks'],
       resources: ['*'],
       effect: iam.Effect.ALLOW,
     }))
-    // cdk deploy needs to assume the CDK deployment role (https://docs.aws.amazon.com/cdk/v2/guide/bootstrapping.html)
+
+    // We have more than one stack to be deployed into potentially different accounts
+    // cdk deploy needs to assume the CDK deployment role in each account
+    // (https://docs.aws.amazon.com/cdk/v2/guide/bootstrapping.html)
     cdkDeployProject.role?.addToPrincipalPolicy(new iam.PolicyStatement({
       actions: ['sts:AssumeRole'],
-      resources: [`arn:*:iam::${this.account}:role/*`, `arn:*:iam::${targetAccount}:role/*`],
+      resources: [
+        `arn:*:iam::${this.account}:role/*`,
+        ...([...targetAccounts].map(accountId => `arn:*:iam::${accountId}:role/*`))
+      ],
       conditions: {
         'ForAnyValue:StringEquals': {
           'iam:ResourceTag/aws-cdk:bootstrap-role': ['image-publishing', 'file-publishing', 'deploy'],
@@ -132,7 +149,7 @@ export class PipelineStack extends Stack {
     })
 
     const unitTestProject = new codeBuild.PipelineProject(this, 'UnitTests', {
-      projectName: `${config.appName}-${stage}-unit-tests`,
+      projectName: `${config.appName}-${lastStage}-unit-tests`,
       buildSpec: codeBuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
@@ -155,59 +172,74 @@ export class PipelineStack extends Stack {
       })]
     })
 
-    const deployActions: codePipelineActions.CodeBuildAction[] = []
-    modules.moduleOrder.forEach((moduleNames, index) => {
-      moduleNames.forEach(moduleName => {
-        const moduleDeployProject = new codeBuild.PipelineProject(
-          this,
-          `${moduleName}_${stage}_deploy`, {
-          projectName: `${moduleName}-${stage}-deploy`,
-          buildSpec: codeBuild.BuildSpec.fromObject({
-            version: '0.2',
-            phases: {
-              install: { commands: ['bash build-scripts/build-module.sh'] },
-              build: { commands: ['bash build-scripts/deploy-module.sh'] },
-            },
-            artifacts: { files: '**/*' }
-          }),
-          environment: codeBuildEnvironment,
-          environmentVariables: {
-            MODULE_NAME: {
-              type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
-              value: moduleName
-            },
-            SLIC_STAGE: {
-              type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
-              value: stage
-            },
-            TARGET_REGION: {
-              type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
-              value: this.region // TODO use target region in context
-            },
-            ROLE_ARN: {
-              type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
-              value: props.crossAccountDeployRole.roleArn
-            }
-          },
-        })
-        moduleDeployProject.role?.addToPrincipalPolicy(new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ['sts:AssumeRole'],
-          resources: [props.crossAccountDeployRole.roleArn]
-        }))
+    stages.forEach((stage, index) => {
+      const targetRegion = this.node.tryGetContext(`${stage}-region`) || this.region
+      const deployActions: codePipelineActions.CodeBuildAction[] = []
 
-        const moduleDeployAction = new codePipelineActions.CodeBuildAction({
-          actionName: `${moduleName}_${stage}_deploy`,
-          project: moduleDeployProject,
-          input: sourceOutput,
-          runOrder: index + 1
+      modules.moduleOrder.forEach((moduleNames, index) => {
+        moduleNames.forEach(moduleName => {
+          const moduleDeployProject = new codeBuild.PipelineProject(
+            this,
+            `${moduleName}_${stage}_deploy`, {
+            projectName: `${moduleName}-${stage}-deploy`,
+            buildSpec: codeBuild.BuildSpec.fromObject({
+              version: '0.2',
+              phases: {
+                install: { commands: ['bash build-scripts/build-module.sh'] },
+                build: { commands: ['bash build-scripts/deploy-module.sh'] },
+              },
+              artifacts: { files: '**/*' }
+            }),
+            environment: codeBuildEnvironment,
+            environmentVariables: {
+              MODULE_NAME: {
+                type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
+                value: moduleName
+              },
+              SLIC_STAGE: {
+                type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
+                value: stage
+              },
+              TARGET_REGION: {
+                type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
+                value: targetRegion
+              },
+              ROLE_ARN: {
+                type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
+                value: props.crossAccountDeployRoles[stage].roleArn
+              }
+            },
+          })
+          moduleDeployProject.role?.addToPrincipalPolicy(new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ['sts:AssumeRole'],
+            resources: [props.crossAccountDeployRoles[stage].roleArn]
+          }))
+
+          const moduleDeployAction = new codePipelineActions.CodeBuildAction({
+            actionName: `${moduleName}_${stage}_deploy`,
+            project: moduleDeployProject,
+            input: sourceOutput,
+            runOrder: index + 1
+          })
+          deployActions.push(moduleDeployAction)
         })
-        deployActions.push(moduleDeployAction)
       })
-    })
-    pipeline.addStage({
-      stageName: 'DeployModules',
-      actions: deployActions
+      pipeline.addStage({
+        stageName: `${stage}Deploy`,
+        actions: deployActions
+      })
+
+      if (index < stages.length - 1) {
+        const nextStage = stages[index + 1]
+        pipeline.addStage({
+          stageName: `${nextStage}Approve`,
+          actions: [new codePipelineActions.ManualApprovalAction({
+            actionName: `${nextStage}Approve`,
+            // notificationTopic: TBD
+          })]
+        })
+      }
     })
   }
 }
