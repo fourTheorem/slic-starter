b4445ae8771225f9965cfd7c0b1537b2e7c397a8
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-12-08 09:09:27 +0000

    Add module-specific builds back (for front end)

--- build-scripts/build-module.sh
+++ build-scripts/build-module.sh
@@ -4,20 +4,9 @@ set -e
 
 source build-scripts/assume-cross-account-role.env
 
-if [ -e package.json ]; then
-  npm ci
-else
-  echo No root package.json, skipping npm ci
-fi
-
+# This script assumes that `npm ci` or `npm install` has already been run in the root and the module
+# Some modules may require a module-specific build
 cd ${MODULE_NAME}
-if [ -e package.json ]; then
-  npm ci
-else
-  echo No module package.json, skipping npm ci
-fi
-
-# Optional, module-specific build
 if [ -e ./scripts/build.sh ]; then
   bash ./scripts/build.sh
 fi
