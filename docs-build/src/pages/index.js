a86dde7b46bff421a978ed99a377beb984dcc81b
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2022-04-01 18:30:17 +0100

    lint fix

--- docs-build/src/pages/index.js
+++ docs-build/src/pages/index.js
@@ -22,7 +22,7 @@ function HomepageHeader () {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            QUICK START 
+            QUICK START
           </Link>
         </div>
       </div>
