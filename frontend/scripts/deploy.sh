8cecb619983ea8672ede49f19fce1246b62d99a2
direnakkocdemir <drnakkoc94@gmail.com>
2022-04-11 15:33:13 +0100

    Add a script to invalidate the CloudFront cache manually

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
-sls deploy --stage $STAGE
+SCRIPTS_DIR=$(realpath $(dirname $0))
 
-export DISTRIBUTION_ID=$(aws cloudfront list-distributions --query 'DistributionList.Items[0].Id' --output text)
-export INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id=$DISTRIBUTION_ID --paths '/*' --query 'Invalidation.Id' --output text)
-aws cloudfront wait invalidation-completed --distribution-id=$DISTRIBUTION_ID --id=$INVALIDATION_ID --no-cli-pager
-echo 'Deployment and cache invalidation complete :)'
+sls deploy --stage $STAGE --region ${TARGET_REGION}
+
+aws cloudfront create-invalidation --distribution-id ${distribution_ID} --paths "/*"
+
+echo frontend deploy.sh
