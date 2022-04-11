25baa8e803c0f58da30657ea7579ecf08e568733
direnakkocdemir <drnakkoc94@gmail.com>
2022-04-11 17:03:34 +0100

    Modify the deploy.sh - To retrieve the id from list-distributions

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -5,6 +5,6 @@ SCRIPTS_DIR=$(realpath $(dirname $0))
 
 sls deploy --stage $STAGE --region ${TARGET_REGION}
 
-aws cloudfront create-invalidation --distribution-id ${distribution_ID} --paths "/*"
+aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='S3-BUCKET_NAME'].id" --output text
 
 echo frontend deploy.sh
