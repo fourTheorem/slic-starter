a7b5b94b87c5e78da6623cfba35f9923b15a02b3
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-06-13 16:34:52 +0100

    build(sls): upgrade handler runtime to node16

--- cicd/config.ts
+++ cicd/config.ts
@@ -5,15 +5,15 @@ const yaml = require('js-yaml')
 
 let appConfig
 try {
-  appConfig = yaml.load(fs.readFileSync(path.resolve(__dirname, '..', 'app.yml'), 'utf8'));
+  appConfig = yaml.load(fs.readFileSync(path.resolve(__dirname, '..', 'app.yml'), 'utf8'))
 } catch (err) {
   console.log(err)
-  throw new Error(`The application must be configured in app.yml`)
+  throw new Error('The application must be configured in app.yml')
 }
 
 export default {
   nsDomain: appConfig.domainConfig.nsDomain,
-  runtime: 'nodejs14.x',
+  runtime: 'nodejs16.x',
   sourceRepoOwner: appConfig.sourceRepo.owner,
   sourceRepoName: appConfig.sourceRepo.name,
   sourceBranch: appConfig.sourceRepo.branch,
