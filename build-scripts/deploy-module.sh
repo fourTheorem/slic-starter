3241d21e81698b9df46619f6d31c9ad3b7466c73
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-06-14 12:10:13 +0100

    build: revert sls hash update deployment

--- build-scripts/deploy-module.sh
+++ build-scripts/deploy-module.sh
@@ -9,5 +9,4 @@ if [ "${MODULE_NAME}" == "certs" ]; then
   TARGET_REGION="us-east-1"
 fi
 
-../node_modules/serverless/bin/serverless.js deploy --stage ${SLIC_STAGE} --region ${TARGET_REGION} --force --enforce-hash-update --verbose
-../node_modules/serverless/bin/serverless.js deploy --stage ${SLIC_STAGE} --region ${TARGET_REGION} --force --verbose
+../node_modules/serverless/bin/serverless.js deploy --stage ${SLIC_STAGE} --region ${TARGET_REGION} --force
