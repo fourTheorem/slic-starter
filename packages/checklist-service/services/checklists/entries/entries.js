1ebfd546d585927c6c664977d5c95e4b07742a7d
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-06-28 17:44:10 +0100

    feat(checklist-service): migrate entries db ops to aws sdk v3

--- packages/checklist-service/services/checklists/entries/entries.js
+++ packages/checklist-service/services/checklists/entries/entries.js
@@ -1,47 +1,38 @@
+const {
+  GetCommand,
+  UpdateCommand
+} = require('@aws-sdk/lib-dynamodb')
 const { v4: uuid } = require('uuid')
 const { dynamoDocClient } = require('slic-tools/aws')
 
 const { createMetricsLogger, Unit } = require('aws-embedded-metrics')
 
-const tableName = process.env.CHECKLIST_TABLE_NAME
-
-module.exports = {
-  addEntry,
-  updateEntry,
-  listEntries,
-  deleteEntry
-}
+const TableName = process.env.CHECKLIST_TABLE_NAME
+const dynamo = dynamoDocClient()
 
 async function addEntry ({ userId, listId, title, value }) {
   const entId = uuid()
   const params = {
-    TableName: tableName,
-
+    TableName,
     Key: {
       userId,
       listId
     },
-
     UpdateExpression: 'SET #ent.#entId = :entry',
     ExpressionAttributeNames: {
       '#ent': 'entries',
       '#entId': entId
     },
-
     ExpressionAttributeValues: {
       ':entry': {
         title,
         value
       }
     },
-
     ReturnValues: 'ALL_NEW'
   }
-  const {
-    Attributes: { entries }
-  } = await dynamoDocClient()
-    .update(params)
-    .promise()
+
+  const { Attributes: { entries } } = await dynamo.send(new UpdateCommand(params))
 
   const metrics = createMetricsLogger()
   metrics.putMetric('NumEntries', Object.keys(entries).length, Unit.Count)
@@ -57,7 +48,7 @@ async function addEntry ({ userId, listId, title, value }) {
 
 async function updateEntry ({ userId, listId, entId, title, value }) {
   const params = {
-    TableName: tableName,
+    TableName,
     Key: {
       userId,
       listId
@@ -77,9 +68,7 @@ async function updateEntry ({ userId, listId, entId, title, value }) {
     ReturnValues: 'NONE'
   }
 
-  await dynamoDocClient()
-    .update(params)
-    .promise()
+  await dynamo.send(new UpdateCommand(params))
 
   const metrics = createMetricsLogger()
   metrics.putMetric('EntryWords', title.trim().split(/s/).length, Unit.Count)
@@ -93,33 +82,36 @@ async function updateEntry ({ userId, listId, entId, title, value }) {
 }
 
 async function listEntries ({ listId, userId }) {
-  return (await dynamoDocClient()
-    .get({
-      TableName: tableName,
-      Key: { userId, listId },
-      ProjectionExpression: 'entries'
-    })
-    .promise()).Item.entries
+  const params = {
+    TableName,
+    Key: { userId, listId },
+    ProjectionExpression: 'entries'
+  }
+
+  const { Item } = await dynamo.send(new GetCommand(params))
+  return Item.entries
 }
 
 async function deleteEntry ({ userId, listId, entId }) {
   const params = {
-    TableName: tableName,
+    TableName,
     Key: {
       userId,
       listId
     },
-
-    UpdateExpression: 'Remove #ent.#entId',
+    UpdateExpression: 'REMOVE #ent.#entId',
     ExpressionAttributeNames: {
       '#ent': 'entries',
       '#entId': entId
-    },
-
-    ReturnValues: 'ALL_NEW'
+    }
   }
 
-  await dynamoDocClient()
-    .update(params)
-    .promise()
+  await dynamo.send(new UpdateCommand(params))
+}
+
+module.exports = {
+  addEntry,
+  updateEntry,
+  listEntries,
+  deleteEntry
 }
