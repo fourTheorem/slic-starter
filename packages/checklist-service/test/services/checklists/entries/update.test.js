aa3eef716142e8ac0c05737ceb91f5283f1d1e4a
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-06-29 12:52:50 +0100

    test(checklist-service): refactor entries tests and remove use of proxyquire

--- packages/checklist-service/test/services/checklists/entries/update.test.js
+++ packages/checklist-service/test/services/checklists/entries/update.test.js
@@ -1,45 +1,53 @@
-const proxyquire = require('proxyquire')
-const { test } = require('tap')
+const t = require('tap')
+const { v4: uuid } = require('uuid')
 
 const { userId, userRequestContext } = require('../../../fixtures')
 
-const received = {}
-
-const updateEntryHandler = proxyquire(
-  '../../../../services/checklists/entries/update',
-  {
-    './entries': {
-      updateEntry: params => {
-        received.updateEntryParams = params
-        return received
-      }
+let updateEntryParams = {}
+let updateEntryReturnVal = {}
+const updateEntryHandler = t.mock('../../../../services/checklists/entries/update', {
+  '../../../../services/checklists/entries/entries.js': {
+    updateEntry: params => {
+      updateEntryParams = { ...params }
+      return Promise.resolve(updateEntryReturnVal)
     }
   }
-)
+})
 
-test('update entry handler allows updating of checklist entries', async t => {
+t.beforeEach(async () => {
+  updateEntryParams = {}
+  updateEntryReturnVal = {}
+})
+
+t.test('update entry handler allows updating of checklist entries', async t => {
+  const listId = uuid()
+  const entId = uuid()
   const event = {
     requestContext: userRequestContext,
     pathParameters: {
-      id: '1234',
-      entId: '1a42'
+      id: listId,
+      entId
     },
     body: JSON.stringify({
       title: 'new value',
       value: 'complete'
     })
   }
+  updateEntryReturnVal = { entId, title: 'new value', value: 'complete' }
 
   const result = await updateEntryHandler.main(event)
 
-  t.same(received.updateEntryParams, {
-    listId: '1234',
-    entId: '1a42',
+  t.same(updateEntryParams, {
+    listId,
+    entId,
     title: 'new value',
     value: 'complete',
     userId
   })
   t.equal(result.statusCode, 200)
-
-  t.end()
+  t.same(JSON.parse(result.body), {
+    entId,
+    title: 'new value',
+    value: 'complete'
+  })
 })
