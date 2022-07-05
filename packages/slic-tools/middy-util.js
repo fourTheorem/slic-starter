ed6d79cea337dcd526e7100e1a6bf929dc41a05b
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-07-05 19:16:35 +0100

    feat(email-service): convert package to esm

--- packages/slic-tools/middy-util.js
+++ packages/slic-tools/middy-util.js
@@ -8,39 +8,36 @@ import inputOutputLogger from '@middy/input-output-logger';
 
 import { log } from './log.js';
 
-export function middify(exports, options = {}) {
-  const result = {};
-  Object.keys(exports).forEach((key) => {
-    const handler = middy(exports[key]).use(
-      inputOutputLogger({
-        logger: (request) => {
-          log.debug({ request }, 'request');
+export function middify(handler, options = {}) {
+  const innerHandler = middy(handler).use(
+    inputOutputLogger({
+      logger: (request) => {
+        log.debug({ request }, 'request');
+      },
+    })
+  );
+
+  if (options.isHttpHandler) {
+    innerHandler.use([
+      httpEventNormalizer(),
+      httpJsonBodyParser(),
+      httpCors(),
+      httpErrorHandler(),
+    ]);
+  }
+
+  /* istanbul ignore next */
+  if (options.ssmParameters && process.env.SLIC_STAGE !== 'test') {
+    innerHandler.use(
+      ssm({
+        fetchData: options.ssmParameters,
+        awsClientOptions: {
+          endpoint: process.env.SSM_ENDPOINT_URL,
         },
+        setToContext: true,
       })
     );
+  }
 
-    if (options.isHttpHandler) {
-      handler.use([
-        httpEventNormalizer(),
-        httpJsonBodyParser(),
-        httpCors(),
-        httpErrorHandler(),
-      ]);
-    }
-
-    /* istanbul ignore next */
-    if (options.ssmParameters && process.env.SLIC_STAGE !== 'test') {
-      handler.use(
-        ssm({
-          fetchData: options.ssmParameters,
-          awsClientOptions: {
-            endpoint: process.env.SSM_ENDPOINT_URL,
-          },
-          setToContext: true,
-        })
-      );
-    }
-    result[key] = handler;
-  });
-  return result;
+  return innerHandler;
 }
