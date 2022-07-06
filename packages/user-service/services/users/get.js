d33c9e748f6c21bc5d75c3c0c02ab2f72c761fe0
Gurarpit Singh <3026722+gsingh1@users.noreply.github.com>
2022-07-06 15:06:33 +0100

    refactor(user-service): dont use toplevel await with sls bundle

--- packages/user-service/services/users/get.js
+++ packages/user-service/services/users/get.js
@@ -1,14 +1,19 @@
 import { createResponse, processEvent, middify } from 'slic-tools';
-
-const user = process.env.IS_OFFLINE
-  ? await import('./user-offline.js')
-  : await import('./user.js');
+import { get } from './user.js';
 
 async function innerHandler(event) {
   const { pathParameters } = processEvent(event);
   const { id: userId } = pathParameters;
 
-  return createResponse(user.get({ userId }));
+  let user;
+  if (process.env.IS_OFFLINE) {
+    const { get: getUserOffline } = await import('./user-offline.js');
+    user = getUserOffline({ userId });
+  } else {
+    user = await get({ userId });
+  }
+
+  return createResponse(user);
 }
 
 export const handler = middify(innerHandler, { isHttpHandler: true });
