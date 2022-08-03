99f46ca7f0419f2ac5f7c307e276ea86fef60211
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2022-08-03 11:27:29 +0100

    chore: generate CSV from new DynamoDB records

---
+++ packages/checklist-service/manual-test/test-firehose.js
@@ -0,0 +1,6 @@
+import { readFileSync } from 'node:fs';
+import { handleTransformer } from '../services/checklists/transformer.js';
+
+const eventJson = readFileSync('./test-events/firehose-event.json');
+const event = JSON.parse(eventJson);
+handleTransformer(event);
--- packages/checklist-service/services/checklists/transformer.js
+++ packages/checklist-service/services/checklists/transformer.js
@@ -2,96 +2,47 @@ import { log } from 'slic-tools';
 
 log.info('Loading function');
 
-/* Stock Ticker format parser */
-const parser =
-  /^{"ticker_symbol":"[a-z]+","sector":"[a-z]+","change":[\d.-]+,"price":[\d.-]+}/i;
-// "ticker_symbol":"NGC","sector":"HEALTHCARE","change":-0.08,"price":4.73
-
-export async function handleTransformer(event, context, callback) {
-  // eslint-disable-next-line no-unused-vars
-  let successs = 0; // Number of valid entries found
-  // eslint-disable-next-line no-unused-vars
+export function handleTransformer(event) {
+  log.debug({ event: JSON.stringify(event) }, 'Event');
+  let success = 0; // Number of valid entries found
   let failure = 0; // Number of invalid entries found
-  // eslint-disable-next-line no-unused-vars
   let dropped = 0; // Number of dropped entries
 
   /* Process the list of records and transform them */
   const output = event.records.map((record) => {
     const entry = Buffer.from(record.data, 'base64').toString('utf8');
-    log.info(entry, 'Entry:');
-    const match = parser.exec(entry);
-    if (match) {
-      const parsedMatch = JSON.parse(match);
-      const milliseconds = Date.now();
-      const parseMatchString = JSON.stringify(parsedMatch);
-      log.info(parseMatchString, 'Data logged');
-      /* Add timestamp and convert to CSV */
-      const result =
-        `${milliseconds},${parsedMatch.ticker_symbol},${parsedMatch.sector},${parsedMatch.change},${parsedMatch.price}` +
-        '\n';
-      const payload = Buffer.from(result, 'utf8').toString('base64');
-      if (parsedMatch.sector !== 'RETAIL') {
-        /* Dropped event, notify and leave the record intact */
-        log.info(`Dropped event :${record.data}`);
-        dropped = +1;
+    try {
+      const message = JSON.parse(entry);
+      if (message.dynamodb.NewImage) {
+        const {
+          listId: { S: listId },
+          userId: { S: userId },
+        } = message.dynamodb.NewImage;
+        const csvRecord = `${listId},${userId}\n`;
+        success += 1;
         return {
           recordId: record.recordId,
-          result: 'Dropped',
-          data: record.data,
+          result: 'Ok',
+          data: Buffer.from(csvRecord, 'utf8').toString('base64'),
         };
       }
-      /* Transformed event */
-      log.info(`Successfull event :${payload}`);
-      successs = +1;
+      dropped += 1;
       return {
         recordId: record.recordId,
-        result: 'Ok',
-        data: payload,
+        result: 'Dropped',
+        data: entry,
+      };
+    } catch (error) {
+      log.error({ err: error, entry }, 'Failed to process record');
+      failure += 1;
+      return {
+        recordId: record.recordId,
+        result: 'ProcessingFailed',
+        data: entry,
       };
     }
-    /* Failed event, notify the error and leave the record intact */
-    log.info(`Failed event :${record.data}`);
-    failure = +1;
-    return {
-      recordId: record.recordId,
-      result: 'ProcessingFailed',
-      data: record.data,
-    };
   });
-  log.info(`Processing completed.  Successful records ${output.length}.`);
-  callback(undefined, { records: output });
+
+  log.info({ success, failure, dropped }, 'Processing completed');
+  return { records: output };
 }
-
-// export async function handleTransformer(event) {
-//   let success = 0; // Number of valid entries found
-//   let failure = 0; // Number of invalid entries found
-//   let dropped = 0; // Number of dropped entries
-
-//   /* Process the list of records and transform them */
-//   const output = event.records.map((record) => {
-//     // Extract JSON record from base64 data
-//     const buffer = Buffer.from(record.data, 'base64').toString();
-//     const jsonRecord = JSON.parse(buffer);
-//     const jsonRecordString = JSON.stringify(jsonRecord);
-//     log.info({ jsonRecordString }, 'Data logged');
-
-//     // Add calculated field
-//     jsonRecord.output =
-//       ((jsonRecord.cadence + 35) * (jsonRecord.resistance + 65)) / 100;
-
-//     // Convert back to base64 + add a newline
-//     const dataBuffer = Buffer.from(
-//       `${JSON.stringify(jsonRecord)}\n`,
-//       'utf8'
-//     ).toString('base64');
-
-//     return {
-//       recordId: record.recordId,
-//       result: 'Ok',
-//       data: dataBuffer,
-//     };
-//   });
-
-//   log.info(`{ recordsTotal: ${output.length} }`);
-//   return { records: output };
-// }
